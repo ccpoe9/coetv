21473061c224d4bb4f02fe54909dcdbd33d766fd
ccpoe9 <chikapaulezeh@gmail.com>
2022-07-21 15:23:23 -0400

    Add video files to db

--- DB/CreateDatabase.sql
+++ DB/CreateDatabase.sql
@@ -1,2 +1,17 @@
 DROP DATABASE IF EXISTS `video-website-db`;
 CREATE DATABASE `video-website-db`;
+
+CREATE TABLE `video-website-db`.`Movies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NULL,
+  `Genre` VARCHAR(45) NULL,
+  `FileRef` VARCHAR(45) NULL,
+  `Desc` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`));
+  
+#SELECT * FROM `video-website-db`.Movies;
+  
+INSERT INTO `video-website-db`.Movies(`Name`,`Genre`,`FileRef`,`Desc`) 
+VALUES('Amazing Grace', 'Spiritual','/Video Files/amazinggrace.mp4','I play amazing grace');
+INSERT INTO `video-website-db`.Movies(`Name`,`Genre`,`FileRef`,`Desc`) 
+VALUES('Arpeggios', 'World','/Video Files/ARPEGGIOS.mp4','I play arpeggios');
