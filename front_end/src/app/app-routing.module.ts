c1c89052fa2d6fe0cbc9b186a9fa26056a9aa89a
ccpoe9 <chikapaulezeh@gmail.com>
2022-07-21 09:31:43 -0400

    Added routing

--- front_end/src/app/app-routing.module.ts
+++ front_end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LiveComponent } from './live/live.component';
+import { MoviesComponent } from './movies/movies.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TvComponent } from './tv/tv.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path : '', component : LandingComponent},
+                        {path : 'movies', component : MoviesComponent},
+                        {path : 'tv', component : TvComponent},
+                        {path : 'live', component : LiveComponent},
+                        {path : 'settings', component : SettingsComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvComponent } from './tv/tv.component';
+import { LiveComponent } from './live/live.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingComponent,
+    MoviesComponent,
+    TvComponent,
+    LiveComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
