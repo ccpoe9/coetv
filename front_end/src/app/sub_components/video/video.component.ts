326dc5a6e2edd116407e2d83998115838641fe4f
ccpoe9 <chikapaulezeh@gmail.com>
2022-08-16 19:42:02 -0400

    video tv update

--- front_end/src/app/sub_components/video/video.component.ts
+++ front_end/src/app/sub_components/video/video.component.ts
@@ -18,6 +18,7 @@ export class VideoComponent implements OnInit {
   video : any;
   recommended : any[];
   httpParams : HttpParams;
+  type : string;
   constructor(private router : Router, private routerService : RouterService, private movieService : MoviesService,
               private tvservice : TvService) { 
   
@@ -29,13 +30,16 @@ export class VideoComponent implements OnInit {
     }
     if(this.router.url.charAt(9) == 'm'){
       this.movieService.getMovie(this.router.url).subscribe( data => {
+        this.type = 'm';
         this.video = data[0];
         this.getMoviesLikeThis(this.video.Genre);
       })
     }
     else if(this.router.url.charAt(9) == 's'){
       this.tvservice.getShow(this.router.url).subscribe( data => {
+        this.type = 's';
         this.video = data[0];
+        this.getShowsLikeThis(this.video.Genre);
       })
     }
 
@@ -58,5 +62,24 @@ export class VideoComponent implements OnInit {
     });
 
   }
+
+  getShowsLikeThis(genre : string){
+    this.httpParams = new HttpParams()
+      .set('currentPage', 1)
+      .set('size', 6)
+      .set('search', '')
+      .set('genre', genre)
+      .set('orderBy', 'Rating')
+      .set('orderDir', 'DESC');
+
+    this.tvservice.getAllShows(this.httpParams).subscribe( data => {
+      this.recommended = data[0].filter( (item : any ) => { return item.Name != this.video.Name});
+      if(this.recommended.length > 5) { 
+        this.recommended.pop();
+      }
+    });
+
+  }
+
 
 }
