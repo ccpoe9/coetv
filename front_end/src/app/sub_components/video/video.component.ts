0166a05ccc8f0b93f045725ab8bca80b91cd4311
ccpoe9 <chikapaulezeh@gmail.com>
2022-08-20 19:48:55 -0400

    commit i thought happened yrs ago

--- front_end/src/app/sub_components/video/video.component.ts
+++ front_end/src/app/sub_components/video/video.component.ts
@@ -1,5 +1,5 @@
 import { HttpParams } from '@angular/common/http';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, DEFAULT_CURRENCY_CODE, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router, RoutesRecognized } from '@angular/router';
 import { filter, pairwise, take } from 'rxjs';
 import { Episode } from 'src/app/models/episode.model';
@@ -8,6 +8,7 @@ import { MoviesService } from 'src/app/services/movies.service';
 import { RouterService } from 'src/app/services/router.service';
 import { TvService } from 'src/app/services/tv.service';
 import { trigger, transition, animate, style } from '@angular/animations';
+import { Tv } from 'src/app/models/tv.model';
 
 @Component({
   selector: 'app-video',
@@ -27,8 +28,12 @@ import { trigger, transition, animate, style } from '@angular/animations';
 })
 export class VideoComponent implements OnInit {
 
-  previousUrl : string;
-  video : any;
+  movie : Movie;
+  show : Tv;
+  Name : string;
+  Desc : string;
+  Genre : string;
+  source : string;
   recommended : any[];
   episodes : Episode[];
   seasons : number[] = [];
@@ -51,22 +56,30 @@ export class VideoComponent implements OnInit {
     if(this.router.url.charAt(9) == 'm'){
       this.movieService.getMovie(this.router.url).subscribe( data => {
         this.type = 'm';
-        this.video = data[0];
-        this.getMoviesLikeThis(this.video.Genre);
+        this.movie = data[0];
+        this.Name = this.movie.Name;
+        this.Desc = this.movie.Desc;
+        this.Genre = this.movie.Genre;
+        this.source = this.movie.Video;
+        this.getMoviesLikeThis(this.movie.Genre);
       })
     }
     else if(this.router.url.charAt(9) == 's'){
       this.tvservice.getShow(this.router.url).subscribe( data => {
         this.type = 's';
-        this.video = data[0][0];
+        this.show = data[0][0];
+        this.Name = this.show.Name;
+        this.Genre = this.show.Genre;
         this.totalSeasons = data[2][0].totalSeasons;
         this.setSeasons(this.totalSeasons);
-        this.constructParams(this.video.Name,1);
+        this.constructParams(this.show.Name,1);
         this.tvservice.getShowSeason(this.httpParams).subscribe( data => {
           this.episodes = data[0];
+          this.Desc = this.episodes[0].Desc;
+          this.source = this.episodes[0].Video;
           this.currentEpisode = this.episodes[0];
         });
-        this.getShowsLikeThis(this.video.Genre);
+        this.getShowsLikeThis(this.show.Genre);
       })
     }
 
@@ -86,7 +99,7 @@ export class VideoComponent implements OnInit {
 
   changeSeason(season : number){
     this.currentSeason = season;
-    this.constructParams(this.video.Name,season);
+    this.constructParams(this.show.Name,season);
     this.tvservice.getShowSeason(this.httpParams).subscribe( data => {
       this.episodes = data[0];
     });
@@ -94,6 +107,7 @@ export class VideoComponent implements OnInit {
 
   changeEpisode(episode : Episode){
     this.currentEpisode = episode;
+    this.source = episode.Video;
   }
 
   getMoviesLikeThis(genre : string){
@@ -106,7 +120,7 @@ export class VideoComponent implements OnInit {
       .set('orderDir', 'DESC');
 
     this.movieService.getAllMovies(this.httpParams).subscribe( data => {
-      this.recommended = data[0].filter( (item : any ) => { return item.Name != this.video.Name});
+      this.recommended = data[0].filter( (item : any ) => { return item.Name != this.movie.Name});
       if(this.recommended.length > 5) { 
         this.recommended.pop();
       }
@@ -124,7 +138,7 @@ export class VideoComponent implements OnInit {
       .set('orderDir', 'DESC');
 
     this.tvservice.getAllShows(this.httpParams).subscribe( data => {
-      this.recommended = data[0].filter( (item : any ) => { return item.Name != this.video.Name});
+      this.recommended = data[0].filter( (item : any ) => { return item.Name != this.show.Name});
       if(this.recommended.length > 5) { 
         this.recommended.pop();
       }
@@ -134,9 +148,7 @@ export class VideoComponent implements OnInit {
   }
 
   playAction(){
-
     this.isPlayed=true;
-
     this.playText = 'PLAYED';
   }
 
