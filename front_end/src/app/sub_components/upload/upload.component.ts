bc5767541fc8215384f496e0834266e085077647
ccpoe9 <chikapaulezeh@gmail.com>
2022-08-22 15:34:40 -0400

    upload component

--- front_end/src/app/sub_components/upload/upload.component.ts
+++ front_end/src/app/sub_components/upload/upload.component.ts
@@ -1,4 +1,10 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie.model';
+import { Tv } from 'src/app/models/tv.model';
+import { MoviesService } from 'src/app/services/movies.service';
+import { TvService } from 'src/app/services/tv.service';
+import { MoviesComponent } from '../movies/movies.component';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +13,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private moviesService : MoviesService, private tvservice : TvService) { }
+
+  ContentTypes : string[] = ['MOVIES', 'TV SHOWS'];
+  currentContentType : string = this.ContentTypes[0];
+  httpParams : HttpParams;
+
+  movies : Movie[];
+  totalMovieRecords : number;
+  totalMoviePages : number;
+  shows : Tv[];
+  totalShowRecords : number;
+  totalShowPages : number;
+ 
 
   ngOnInit(): void {
+    this.constructParams(1,20,'','','id','DESC');
+    this.moviesService.getAllMovies(this.httpParams).subscribe(data => {
+      this.movies = data[0];
+      this.totalMoviePages = data[2][0].totalPages;
+      this.totalMovieRecords = data[2][0].totalRecords;
+    });
+    this.tvservice.getAllShows(this.httpParams).subscribe(data => {
+      this.shows = data[0];
+      this.totalShowPages = data[2][0].totalPages;
+      this.totalShowRecords = data[2][0].totalRecords;
+    });
+  }
+  constructParams(currentPage : number,
+    size : number,
+    search : string,
+    genre : string,
+    orderBy : string,
+    orderDir : string){
+    
+      this.httpParams = new HttpParams()
+      .set('currentPage', currentPage)
+      .set('size', size)
+      .set('search', search)
+      .set('genre', genre)
+      .set('orderBy', orderBy)
+      .set('orderDir', orderDir);
+  }
+  getAllMovies(){
+    this.moviesService.getAllMovies(this.httpParams).subscribe(data => {
+      this.movies = data[0];
+      this.totalMoviePages = data[2][0].totalPages;
+      this.totalMovieRecords = data[2][0].totalRecords;
+      
+  });
+  }
+
+  getAllShows(){
+    this.tvservice.getAllShows(this.httpParams).subscribe(data => {
+      this.shows = data[0];
+      this.totalShowPages = data[2][0].totalPages;
+      this.totalShowRecords = data[2][0].totalRecords;
+  });
+  }
+
+  changeContentType( type : string){
+    this.currentContentType = type;
   }
 
 }
