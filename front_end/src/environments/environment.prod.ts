cb972d6cd7f9b9e45a90eda032b98bc87886d3a1
ccpoe9 <chikapaulezeh@gmail.com>
2022-09-27 19:28:05 -0400

    front_end deployment (update 2)

--- front_end/src/environments/environment.prod.ts
+++ front_end/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: '...',
-    authDomain: 'project.firebaseapp.com',
-    databaseURL: 'https://project.firebaseio.com',
-    projectId: 'project',
-    storageBucket: 'project.appspot.com',
-    messagingSenderId: '...',
-  },
+  firebase : {
+    apiKey: "AIzaSyA46nVhhwxyJdIt7bAC0EDna9bCYh5DNZg",
+    authDomain: "mediahub-21cb8.firebaseapp.com",
+    projectId: "mediahub-21cb8",
+    storageBucket: "mediahub-21cb8.appspot.com",
+    messagingSenderId: "1095432475265",
+    appId: "1:1095432475265:web:503fdb3112383d85e27380",
+    measurementId: "G-JW1N2ZVZ12"
+  }
 };
