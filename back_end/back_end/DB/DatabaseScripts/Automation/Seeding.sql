a54da808013e80ee8e4e348f9041562cea38277a
Christian Okey-Ezeh <chikapaulezeh@gmail.com>
2022-10-01 13:38:56 +0000

    connecting droplet to git

---
+++ back_end/back_end/DB/DatabaseScripts/Automation/Seeding.sql
@@ -0,0 +1,57 @@
+#MOVIES
+INSERT INTO `mediatime-db`.Movies(`Name`,`Genre`,`Thumbnail`,`Video`,`Desc`,`Rating`,`URL`, `Trailer`) 
+VALUES('Big Buck Bunny','Comedy,Animation','https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/1200px-Big_buck_bunny_poster_big.jpg','http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4','A day in the life of Big Buck Bunny', 6.5,'Mjk1NDE=','_xQ-t7vFDcY');
+INSERT INTO `mediatime-db`.Movies(`Name`,`Genre`,`Thumbnail`,`Video`,`Desc`,`Rating`,`URL`, `Trailer`) 
+VALUES('Tarzan','Animation,Adventure,Comedy','https://m.media-amazon.com/images/M/MV5BY2ZiYWUxN2ItYmQxZi00NDlkLWE2NDAtOTNmYTg1MDI0NDk1XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg','https://s3.us-east-1.wasabisys.com/coemediatime/Movies/Tarzan (1999)/Tarzan.1999.720p.mp4','A man raised by gorillas must decide where he really belongs when he discovers he is a human.', 7.3,'MTIzNDU=','ie53R2HEZ6g');
+
+
+#GENRES 
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Comedy');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Animation');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Action');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Adventure');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Anime');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Horror');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Fantasy');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Thriller');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Crime');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Historical');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Romance');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Sci-Fi');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Western');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('Other');
+INSERT INTO `mediatime-db`.Genres(`Name`)
+VALUES('TEST');
+
+
+#SHOWS
+
+
+
+#LIVE
+INSERT INTO `mediatime-db`.Live(`Name`,`EPGID`,`Thumbnail`,`Source`)
+VALUES('Comedy Central Pluto TV', '5ca671f215a62078d2ec0abf','https://images.pluto.tv/channels/5ca671f215a62078d2ec0abf/colorLogoPNG.png',
+'https://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/5ca671f215a62078d2ec0abf/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channe
+l&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel
+&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS&already_redirected=true');
+INSERT INTO `mediatime-db`.Live(`Name`,`EPGID`,`Thumbnail`,`Source`)
+VALUES('Nick Pluto TV', '5ca673e0d0bd6c2689c94ce3','https://images.pluto.tv/channels/5ca673e0d0bd6c2689c94ce3/colorLogoPNG.png',
+'http://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5ca673e0d0bd6c2689c94ce3livestitch/master.m3u8?advertisingId=&appName=web&appStoreUrl=&appVersion=DNT&app_name=&architecture=&buildVersion=&deviceDNT=0&deviceId=5ca673e0d0bd6c2689c94ce3&deviceLat=&deviceLon=&deviceMake=web&deviceModel=web&deviceType=web&deviceVersion=DNT&includeExtendedEvents=false&marketingRegion=US&serverSideAds=false&sid=977&terminate=false&userId=&profilesFromStream=true');
+
+ 
+
+
---
+++ back_end/back_end/controllers/shows.controller.js
@@ -0,0 +1,264 @@
+var db = require('../config/db.config');
+const Joi = require('joi');
+const { validateParamsGetMoviesByPage, validateParamsGetShowByUrl, validateParamsGetEpisodesByShowSeason,
+     validateParamsPostShow , validateParamsPostEpisode, validateParamsUpdateShow, validateParamsUpdateEpisode, 
+     validateParamsDeleteShow,  validateParamsDeleteEpisode} = require('../validation/validator');
+const { urlGenerator } = require('../generators/urlGenerate');
+
+exports.GetShowsByPage = (req,res) => {
+
+    const { error, value } = validateParamsGetMoviesByPage(req.query);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let GetShowsByPage = 
+    `CALL GetShowsByPage(${req.query.currentPage},${req.query.size},'${req.query.search}','${req.query.genre}','${req.query.orderBy}','${req.query.orderDir}', @totalRecords, @totalPages);
+     SELECT @totalRecords as totalRecords, @totalPages as totalPages;`;
+
+    db.query(GetShowsByPage, (err,data,fields) =>{
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.send(data);
+    });
+}
+exports.GetShowByUrl = (req,res) => {
+
+    const { error, value } = validateParamsGetShowByUrl(req.query);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let GetShowByUrl =
+    `CALL GetShowByUrl('${req.query.v}', @totalSeasons);
+    SELECT @totalSeasons as totalSeasons;`;
+
+    db.query(GetShowByUrl, (err,data,fields) =>{
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.send(data);
+    });
+
+
+}
+
+exports.GetEpisodesByShowSeason = (req,res) => {
+
+    const { error, value } = validateParamsGetEpisodesByShowSeason(req.query);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let GetEpisodesByShowSeason =
+    `CALL GetEpisodesByShowSeason('${req.query.showName}',${req.query.season}, @totalEpisodes);
+     SELECT @totalEpisodes as totalEpisodes;`;
+    db.query(GetEpisodesByShowSeason, (err,data,fields) =>{
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.send(data);
+    }); 
+}
+
+exports.PostShow = (req,res) => {
+
+    const { error, value } = validateParamsPostShow(req.body);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+    let PostShow =
+    `CALL InsShow('${req.body.Name}','${req.body.Genre}','${req.body.Thumbnail}','${req.body.Desc}', ${req.body.Rating},'${urlGenerator('show')}','${req.body.Trailer}');`;
+
+    db.query(PostShow, (err,data,fields) => {
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.statusMessage = "POST SUCCESFUL";
+        res.status(200).end();
+    });
+}
+
+exports.UpdateShow = (req,res) => {
+
+    const { error, value } = validateParamsUpdateShow(req.body);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let UpdateShow =
+    `SET SQL_SAFE_UPDATES = 0;
+    CALL UpdShow(${req.body.id},'${req.body.Name}','${req.body.Genre}', '${req.body.Thumbnail}','${req.body.Desc}',${req.body.Rating},'${req.body.Trailer}');
+    SET SQL_SAFE_UPDATES = 1;`;
+
+    db.query(UpdateShow, (err,data,fields) => {
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.statusMessage = "PUT SUCCESFUL";
+        res.status(200).end();
+    });
+}
+
+exports.PostEpisode = async (req,res) => {
+
+    totalSeasons  = await getLatestSeason(req.body.showName);
+    
+    var totalEpisodes = await getTotalEpisodes(req.body.showName, req.body.season);
+
+    if(req.body.season > totalSeasons + 1 || !totalSeasons && req.body.season > 1){
+        res.statusMessage = "Input Validation Error : Cannot insert seasons out of order";
+        return res.status(400).end();
+    }
+    const { error, value } = validateParamsPostEpisode(req.body);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+
+    let PostEpisode =
+    `CALL InsEpisode('${req.body.Name}', '${req.body.showName}',${req.body.season},${totalEpisodes+1},'${req.body.Video}','${req.body.Desc}');`;
+
+    db.query(PostEpisode, (err,data,fields) => {
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.statusMessage = "POST SUCCESFUL";
+        res.status(200).end();
+    });
+}
+
+exports.UpdateEpisode = (req,res) => {
+
+    const { error, value } = validateParamsUpdateEpisode(req.body);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let UpdateEpisode = 
+    `CALL UpdEpisode(${req.body.id},'${req.body.Name}','${req.body.Video}', '${req.body.Desc}');`;
+
+    db.query(UpdateEpisode, (err,data,fields) => {
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.statusMessage = "PUT SUCCESFUL";
+        res.status(200).end();
+    });
+}
+
+exports.DeleteShow = (req,res) => {
+
+    const { error, value } = validateParamsDeleteShow(req.query);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let DeleteShow = 
+    `SET SQL_SAFE_UPDATES = 0;
+    CALL DeleteShow(${req.query.id});
+    SET SQL_SAFE_UPDATES = 1;`;
+
+    db.query(DeleteShow, (err,data,fields) => {
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.statusMessage = "DELETE SUCCESFUL";
+        res.status(200).end();
+    });
+}
+exports.DeleteEpisode = (req,res) => {
+
+    const { error, value } = validateParamsDeleteEpisode(req.query);
+    if(error){
+        console.log(error);
+        res.statusMessage = "Input Validation Error : " + error.details[0].message;
+        return res.status(400).end();
+    }
+
+    let DeleteEpisode = 
+    `CALL DeleteEpisode(${req.query.id});`;
+
+    db.query(DeleteEpisode, (err,data,fields) => {
+        if(err){
+            console.error(err.message);
+            res.statusMessage = "SQL Error : " + err.message;
+            return res.status(400).end();
+        }
+        res.statusMessage = "DELETE SUCCESFUL";
+        res.status(200).end();
+    });
+}
+
+
+
+getLatestSeason = async (showName) => {
+
+    let getTotalSeasons =
+    `SET @totalSeasons := 
+    (SELECT DISTINCT(\`Season\`) FROM \`mediatime-db\`.\`Episodes\` e
+        WHERE e.\`ShowName\` = '${showName}'
+        ORDER BY \`Season\` DESC LIMIT 1);
+    SELECT @totalSeasons as totalSeasons;`;
+
+    return new Promise((resolve, reject) => {
+        db.query(getTotalSeasons, (err,data,fields) =>{
+            if(err){
+                console.error(err.message);
+                reject(err.message);
+            }
+            resolve(data[1][0].totalSeasons);
+        });
+    });
+}
+
+
+getTotalEpisodes = async (showName, season) => {
+
+    let GetEpisodesByShowSeason =
+    `CALL GetEpisodesByShowSeason('${showName}',${season}, @totalEpisodes);
+    SELECT @totalEpisodes as totalEpisodes`;
+
+    return new Promise((resolve, reject) => {
+        db.query(GetEpisodesByShowSeason, (err,data,fields) =>{
+            if(err){
+                reject(err.message);
+            }   
+            resolve(data[2][0].totalEpisodes);
+        }); 
+    });
+
+}
