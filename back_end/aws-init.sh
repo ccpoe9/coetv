28e58a2e0c6fadcdcfec0f0d20773754a1a7bbbe
ccpoe9 <chikapaulezeh@gmail.com>
2022-10-04 18:35:23 -0400

    test for aws script(2)

--- back_end/aws-init.sh
+++ back_end/aws-init.sh
@@ -3,5 +3,5 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 aws configure set aws_access_key_id K5FBHAHF4A8811CV1URL
-aws configure set aws_secret_access_key default_secret_key 9XseyYKCYCzVmYrrn1JDAuDItoOtHMI4ITl5O2N3
+aws configure set aws_secret_access_key 9XseyYKCYCzVmYrrn1JDAuDItoOtHMI4ITl5O2N3
 aws configure set default.region us-east-1
